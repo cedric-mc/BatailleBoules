3cd91e8cb7fd37345acc6aeee24aebc23918c4d8
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2024-12-15 17:23:46 +0100

    Refactor button functions and improve docstrings formatting.
    Enhanced `pause_button` and `quit_button` drawing logic;

--- buttons.py
+++ buttons.py
@@ -1,7 +1,7 @@
 # Programmeurs : Cédric Mariya Constantine et Wilson Groevius
 # ------------------------------ Importation depuis le dossier source
-from upemtk import *
-from default import *
+from upemtk import efface, rectangle, texte, ligne, mise_a_jour
+from default import largeur_Fenetre, game_font, b_gauche_x1, b_gauche_x2, b1_y1, b1_y2, txt_gauche_x, txt_y1, b_milieu_y1, b_milieu_y2, txt_y2, b3_y1, b3_y2, txt_y3, b_droit_x1, b_droit_x2, b4_y1, b4_y2, txt_droite_x, b6_y1, b6_y2
 
 
 def button_background(isActive):
@@ -17,6 +17,14 @@ def button_background(isActive):
 
 
 def default_buttons(variantes):
+    """Cette fonction prend en paramètre un dictionnaire de variantes et permet de changer l'apparence des boutons en fonction de la valeur des variantes.
+
+    Args:
+        variantes (dict): Dictionnaire de variantes qui permet de savoir si une variante est activée ou non.
+
+    Returns:
+        variantes (dict): Renvoie un dictionnaire de variantes avec les boutons modifiés.
+    """
     # TODO: Changer l'apparence des boutons pour un carré avec une image en haut et un texte en bas (tout centré et à l'intérieur du carré).
     variantes["sablier"] = f_boutons(True, b_gauche_x1, b_gauche_x2, b1_y1, b1_y2, txt_gauche_x, txt_y1, "Sablier", "sablier", "text_sablier")
     variantes["scores"] = f_boutons(True, b_gauche_x1, b_gauche_x2, b_milieu_y1, b_milieu_y2, txt_gauche_x, txt_y2, "Scores", "scores", "text_scores")
@@ -28,17 +36,22 @@ def default_buttons(variantes):
 
 
 def f_boutons(variante, rec_x1, rec_x2, rec_y1, rec_y2, txt_x, txt_y, txt_in, tag1, tag2):
-    """Cette fonction prend en paramètre une variante booléenne correspond à une des six variantes,
-    rec_x1 : la coordonnée x du point supérieur gauche (première valeur pour dessiner un rectangle) ; type : int.
-    rec_x2 : la coordonnée x du point inférieur droit (quatrième valeur pour dessiner un rectangle) ; type : int.
-    rec_y1 : la coordonnée y du point supérieur gauche (deuxième valeur pour dessiner un rectangle) ; type : int.
-    rec_y2 : la coordonnée y du point inférieur droit (troisième valeur pour dessiner un rectangle) ; type : int.
-    txt_x : position x du texte ; type : int.
-    txt_y : position y du texte ; type : int.
-    txt_in : chaîne de caractères pour indiquer le texte à l'intérieur du bouton.
-    tag1, tag2 : chaînes de caractères correspondant aux identifiants des composantes du bouton (rectangle + texte).
-    Renvoie la variante.
-    La fonction permet de changer l'apparence du bouton."""
+    """Cette fonction prend en paramètre une variante booléenne correspond à une des six variantes et permet de changer l'apparence du bouton.
+    
+    Args:
+        variante (bool): Variable booléenne qui permet de savoir si la variante est activée ou non.
+        rec_x1 (int): La coordonnée x du point supérieur gauche (première valeur pour dessiner un rectangle).
+        rec_x2 (int): La coordonnée x du point inférieur droit (quatrième valeur pour dessiner un rectangle).
+        rec_y1 (int): La coordonnée y du point supérieur gauche (deuxième valeur pour dessiner un rectangle).
+        rec_y2 (int): La coordonnée y du point inférieur droit (troisième valeur pour dessiner un rectangle).
+        txt_x (int): Position x du texte.
+        txt_y (int): Position y du texte.
+        txt_in (str): Chaîne de caractères pour indiquer le texte à l'intérieur du bouton.
+        tag1 (str): Chaînes de caractères correspondant aux identifiants des composantes du bouton (rectangle + texte).
+        tag2 (str): Chaînes de caractères correspondant aux identifiants des composantes du bouton (rectangle + texte).
+        
+    Returns:
+        variante (bool): Renvoie la variante."""
     efface(tag1), efface(tag2)
     variante = not variante
     rectangle(rec_x1, rec_y1, rec_x2, rec_y2, couleur='black', remplissage=button_background(variante), epaisseur=3, tag=tag1)
@@ -47,18 +60,34 @@ def f_boutons(variante, rec_x1, rec_x2, rec_y1, rec_y2, txt_x, txt_y, txt_in, ta
 
 
 def pause_button():
-    rectangle(largeur_Fenetre//2-150, b_milieu_y1, largeur_Fenetre//2+150, b_milieu_y2, couleur='black', remplissage='black', epaisseur=3, tag='pause')
-    texte(largeur_Fenetre//2, txt_y2, "Pause", couleur='white', police=game_font, ancrage='center', tag='text_pause')
+    """Cette fonction permet de dessiner un bouton pause au centre de l'écran de jeu."""
+    # rectangle(largeur_Fenetre//2-150, b_milieu_y1, largeur_Fenetre//2+150, b_milieu_y2, couleur='black', remplissage='black', epaisseur=3, tag='pause')
+    # texte(largeur_Fenetre//2, txt_y2, "Pause", couleur='white', police=game_font, ancrage='center', tag='text_pause')
+    # Dessine le bouton pause
+    rectangle(75, 25, 25, 75, remplissage='green', epaisseur=3, tag='pause')
+    # Coordonnées x et y des 2 traits verticaux
+    x1 = 40 # Coordonnée x du premier point du premier trait vertical
+    y1 = 35 # Coordonnée y du premier point du premier trait vertical
+    y2 = 65 # Coordonnée y du deuxième point du premier trait vertical
+    x2 = 60 # Coordonnée x du deuxième point du deuxième trait vertical
+
+    # Trace les 2 traits verticaux
+    ligne(x1, y1, x1, y2, epaisseur=4, tag="trait1") # Premier trait vertical
+    ligne(x2, y1, x2, y2, epaisseur=4, tag="trait2") # Deuxième trait vertical
     mise_a_jour()
 
 
 def quit_button():
-    rectangle(largeur_Fenetre-75, 25, largeur_Fenetre-25, 75, couleur='black', remplissage='red', epaisseur=3, tag='quit')
-    x1_diag = largeur_Fenetre-70
-    y1_diag = 30
-    x2_diag = largeur_Fenetre-30
-    y2_diag = 70
+    """Cette fonction permet de dessiner un bouton quitter en haut à droite de l'écran de jeu."""
+    # Dessine le bouton quitter
+    rectangle(largeur_Fenetre-75, 25, largeur_Fenetre-25, 75, remplissage='red', epaisseur=3, tag='quit')
+    # Coordonnées x et y des 2 traits diagonaux
+    x1 = largeur_Fenetre-65 # Coordonnée x du premier point du premier trait diagonal
+    y1 = 35 # Coordonnée y du premier point du premier trait diagonal
+    x2 = largeur_Fenetre-35 # Coordonnée x du deuxième point du premier trait diagonal
+    y2 = 65 # Coordonnée y du deuxième point du premier trait diagonal
+
     # Trace les deux traits diagonaux
-    ligne(x1_diag, y1_diag, x2_diag, y2_diag, epaisseur=3, tag="croix1") # Premier trait diagonal
-    ligne(x1_diag, y2_diag, x2_diag, y1_diag, epaisseur=3, tag="croix2") # Deuxième trait diagonal
+    ligne(x1, y1, x2, y2, epaisseur=4, tag="croix1") # Premier trait diagonal
+    ligne(x1, y2, x2, y1, epaisseur=4, tag="croix2") # Deuxième trait diagonal
     mise_a_jour()
